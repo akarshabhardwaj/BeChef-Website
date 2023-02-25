d9ec3519f002b60b8291ae7761bd79aabe29a1a0
rajkumar-cmd <raymarhossor@gmail.com>
2023-02-26 00:15:36 +0530

    route

---
+++ besafe/src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AuthContext } from "../../Context/AuthContextProvider"
+import { Navigate } from "react-router-dom";
+import { useToast,Box } from "@chakra-ui/react";
+
+
+function PrivateRoute({children}) {
+    const toast = useToast()
+    const val=React.useContext(AuthContext);
+    if(val.isAuth===false){
+        return toast({
+            position: 'bottom-left',
+            render: () => (
+              <Box color='white' p={3} bg='blue.500'>
+                Hello World
+              </Box>
+            ),
+          })
+    }
+    else{
+        return children;
+    }
+}
+
+export default PrivateRoute;
